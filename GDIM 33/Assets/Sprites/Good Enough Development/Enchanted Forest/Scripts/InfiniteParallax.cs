ea90315373b35c21cb5943ec904d80ad6595ed3e
yanzhang111 <yanz76@uci.edu>
2026-04-15 16:44:14 -0700

    1

---
+++ GDIM 33/Assets/Sprites/Good Enough Development/Enchanted Forest/Scripts/InfiniteParallax.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class InfiniteParallax : MonoBehaviour
+{
+    private float spriteLength, startpos;
+    public GameObject camera;
+    public float parallaxEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos = transform.position.x;
+        spriteLength = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distanceFromCamera = (camera.transform.position.x * (1 - parallaxEffect));
+        float distanceFromStartPos = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startpos + distanceFromStartPos, transform.position.y, transform.position.z);
+
+        if (distanceFromCamera > startpos + spriteLength)
+        {
+            startpos += spriteLength;
+        }
+        else if (distanceFromCamera < startpos - spriteLength)
+        {
+            startpos -= spriteLength;
+        }
+    }
+}
