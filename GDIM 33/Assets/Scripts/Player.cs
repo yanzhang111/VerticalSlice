18caf1127bac6ed0a25b9945cd9e48097b5e3674
yanzhang111 <yanz76@uci.edu>
2026-04-26 03:15:16 -0700

    1

--- GDIM 33/Assets/Scripts/Player.cs
+++ GDIM 33/Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Move Settings")]
     public float moveSpeed = 5f;
     public float jumpForce = 8f;
 
+    [Header("Ground Check")]
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
@@ -16,6 +18,9 @@ public class Player : MonoBehaviour
     private bool isGrounded;
     private float moveInput;
     private Vector3 originalScale;
+
+    private float jumpStartTime;
+    public float jumpGroundCheckDelay = 0.15f;
 
     void Start()
     {
@@ -28,36 +33,75 @@ public class Player : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        bool canCheckGround = Time.time > jumpStartTime + jumpGroundCheckDelay;
+
+        if (canCheckGround)
+        {
+            isGrounded = Physics2D.OverlapCircle(
+                groundCheck.position,
+                groundCheckRadius,
+                groundLayer
+            );
+        }
+        else
+        {
+            isGrounded = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpStartTime = Time.time;
+            isGrounded = false;
+
+            if (animator != null)
+            {
+                animator.SetTrigger("jump");
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.J) && animator != null)
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            animator.SetTrigger("attack");
+            if (animator != null)
+            {
+                animator.SetTrigger("attack");
+            }
         }
 
         if (animator != null)
         {
             animator.SetBool("isRun", moveInput != 0);
-            animator.SetBool("isJump", !isGrounded);
+            animator.SetBool("isGrounded", isGrounded);
         }
 
         if (moveInput > 0)
         {
-            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+            transform.localScale = new Vector3(
+                Mathf.Abs(originalScale.x),
+                originalScale.y,
+                originalScale.z
+            );
         }
         else if (moveInput < 0)
         {
-            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+            transform.localScale = new Vector3(
+                -Mathf.Abs(originalScale.x),
+                originalScale.y,
+                originalScale.z
+            );
         }
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
